0eb65568ba779036866bc80f2bd522eba647e523
Jmactiag <jessie.macatiag@gmail.com>
2017-12-19 12:10:27 -0800

    clean up js code and fix crystal generator. Now chooses 4 different values all the time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 // declare variables
+// array of possible crystal values
+var crystalValues=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 // random number for the target
 var targetNumber=0;
@@ -26,48 +28,89 @@ function generateTarget(){
 	targetNumber=Math.floor(Math.random()*(120-19)+19);
 	$("#randomNumberDisplay").text(targetNumber);
 }
-generateTarget();
 
-//generate crystal values
+
 function generateCrystals (){
-	crystalOne=Math.floor(Math.random()*(12-1)+1);
-	crystalTwo=Math.floor(Math.random()*(12-1)+1);
-	if (crystalOne===crystalTwo){
-		crystalTwo=Math.floor(Math.random()*(12-1)+1);
+	crystalValues=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	var crystalValueUses=crystalValues;
+	crystalOne=crystalValueUses[Math.floor(Math.random()*crystalValueUses.length)];
+	for (var i=0; i<crystalValueUses.length; i++){
+		if (crystalOne===crystalValueUses[i]){
+			var remove=crystalValueUses.splice(i,1);	
+			console.log(crystalOne);
+		}
 	}
-	crystalThree=Math.floor(Math.random()*(12-1)+1);
-	if (crystalThree===crystalOne || crystalThree===crystalTwo){
-		crystalThree=Math.floor(Math.random()*(12-1)+1);
+
+	crystalTwo=crystalValueUses[Math.floor(Math.random()*crystalValueUses.length)];
+	for (var i=0; i<crystalValueUses.length; i++){
+		if (crystalTwo===crystalValueUses[i]){
+			var remove=crystalValueUses.splice(i,1);
+			console.log(crystalTwo);
+		}
 	}
-	crystalFour=Math.floor(Math.random()*(12-1)+1);
-	if (crystalFour===crystalOne || crystalFour===crystalTwo || crystalFour===crystalThree){
-		crystalFour=Math.floor(Math.random()*(12-1)+1);
+
+	crystalThree=crystalValueUses[Math.floor(Math.random()*crystalValueUses.length)];
+	for (var i=0; i<crystalValueUses.length; i++){
+		if (crystalThree===crystalValueUses[i]){
+			var remove=crystalValueUses.splice(i,1);
+
+			console.log(crystalThree);
+
+		}
 	}
+
+	crystalFour=crystalValueUses[Math.floor(Math.random()*crystalValueUses.length)];
+	for (var i=0; i<crystalValueUses.length; i++){
+		if (crystalFour===crystalValueUses[i]){
+			var remove=crystalValueUses.splice(i,1);
+
+			console.log(crystalFour);
+		}
+	}
+	console.log(crystalValues);
+
+
 }
+
+// win condition function
+function winCondition(){
+	wins++;
+	generateTarget();
+	generateCrystals();
+	totalScore=0;
+	$("#totalScore").text(totalScore);
+	$("#winsNumber").text(wins);
+	
+}
+
+// loss condition funtion
+function lossCondition(){
+	losses++;
+	generateTarget();
+	generateCrystals();
+	totalScore=0;
+	$("#totalScore").text(totalScore);
+	$("#lossesNumber").text(losses);
+
+}
+// gameplay
+
+generateTarget();
 generateCrystals();
 
 
-// gameplay
+
+		
 
 //click first image
 $("#firstCrystal").on("click",function(){
 	totalScore=totalScore+crystalOne;
 	$("#totalScore").text(totalScore);
 	if (totalScore===targetNumber){
-		wins++;
-		generateTarget();
-		generateCrystals();
-		totalScore=0;
-		$("#totalScore").text(totalScore);
-		$("#winsNumber").text(wins);
+		winCondition();
 	}
 	if (totalScore>targetNumber){
-		losses++;
-		generateTarget();
-		generateCrystals();
-		totalScore=0;
-		$("#totalScore").text(totalScore);
-		$("#lossesNumber").text(losses);
+		lossCondition();
 	}
 })
 
@@ -76,20 +119,10 @@ $("#secondCrystal").on("click",function(){
 	totalScore=totalScore+crystalTwo;
 	$("#totalScore").text(totalScore);
 	if (totalScore===targetNumber){
-		wins++;
-		generateTarget();
-		generateCrystals();
-		totalScore=0;
-		$("#totalScore").text(totalScore);
-		$("#winsNumber").text(wins);
+		winCondition();
 	}
 	if (totalScore>targetNumber){
-		losses++;
-		generateTarget();
-		generateCrystals();
-		totalScore=0;
-		$("#totalScore").text(totalScore);
-		$("#lossesNumber").text(losses);
+		lossCondition();
 	}
 })
 
@@ -98,20 +131,10 @@ $("#thirdCrystal").on("click",function(){
 	totalScore=totalScore+crystalThree;
 	$("#totalScore").text(totalScore);
 	if (totalScore===targetNumber){
-		wins++;
-		generateTarget();
-		generateCrystals();
-		totalScore=0;
-		$("#totalScore").text(totalScore);
-		$("#winsNumber").text(wins);
+		winCondition();
 	}
 	if (totalScore>targetNumber){
-		losses++;
-		generateTarget();
-		generateCrystals();
-		totalScore=0;
-		$("#totalScore").text(totalScore);
-		$("#lossesNumber").text(losses);
+		lossCondition();
 	}
 })
 
@@ -120,20 +143,10 @@ $("#fourthCrystal").on("click",function(){
 	totalScore=totalScore+crystalFour;
 	$("#totalScore").text(totalScore);
 	if (totalScore===targetNumber){
-		wins++;
-		generateTarget();
-		generateCrystals();
-		totalScore=0;
-		$("#totalScore").text(totalScore);
-		$("#winsNumber").text(wins);
+		winCondition();
 	}
 	if (totalScore>targetNumber){
-		losses++;
-		generateTarget();
-		generateCrystals();
-		totalScore=0;
-		$("#totalScore").text(totalScore);
-		$("#lossesNumber").text(losses);
+		lossCondition();
 	}
 })
 
